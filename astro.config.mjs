42064bc6f597d6d4ab7ffe22a19d98e5e9cd7f0f
AbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-06-13 11:55:12 +0200

    updated routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site:'https://abertoandrade10.github.io/AlbertoAndrade10.github.io/',
-  base:'/AlbertoAndrade10.github.io/',
+  site: "https://abertoandrade10.github.io/AlbertoAndrade10.github.io/",
+  base: "/AlbertoAndrade10.github.io/",
   vite: {
     plugins: [tailwindcss()],
   },
