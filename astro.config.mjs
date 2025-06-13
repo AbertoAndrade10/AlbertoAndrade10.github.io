d03cbb67b97d2a8dd85205faff4fe8b4d77adbec
AbertoAndrade10 <albertoandradeortiz910@gmail.com>
2025-06-13 10:56:38 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  site:'https://AlbertoAndrade10.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
